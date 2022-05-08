b5f247bf1fc8660bdc10fdbdec764dca537ef7c1
Vadim <artyuhovvadim@yandex.ru>
2022-05-09 01:54:39 +0300

    Исправил ошибку в QuestionDialogWindow

--- LogicSimulator/ViewModels/MainWindowViewModel.cs
+++ LogicSimulator/ViewModels/MainWindowViewModel.cs
@@ -18,9 +18,9 @@ public class MainWindowViewModel : BindableBase
 
     public ICommand TestCommand => _testCommand ??= new LambdaCommand(_ =>
     {
-        //_userDialogService.ShowInfoMessage("Some title", "Some info");
-        //_userDialogService.ShowErrorMessage("Some title", "Some error");
-        //_userDialogService.ShowWarningMessage("Some title", "Some warning");
-        //_userDialogService.ShowQuestionMessage("Some title", "Some question");
+        _userDialogService.ShowInfoMessage("Some title", "Some info");
+        _userDialogService.ShowErrorMessage("Some title", "Some error");
+        _userDialogService.ShowWarningMessage("Some title", "Some warning");
+        _userDialogService.ShowQuestionMessage("Some title", "Some question");
     }, _ => true);
 }
