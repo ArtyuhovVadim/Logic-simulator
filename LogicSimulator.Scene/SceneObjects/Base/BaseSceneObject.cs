0199b165b0ed34b378350f0d1530b899cb8843b3
Vadim <artyuhovvadim@yandex.ru>
2022-06-24 03:18:57 +0300

    Добавил методы для установки и снятия выделения с объекта

--- LogicSimulator.Scene/SceneObjects/Base/BaseSceneObject.cs
+++ LogicSimulator.Scene/SceneObjects/Base/BaseSceneObject.cs
@@ -2,5 +2,11 @@
 
 public abstract class BaseSceneObject : ResourceDependentObject
 {
+    public bool IsSelected { get; protected set; }
+
+    public virtual void Select() => IsSelected = true;
+
+    public virtual void Unselect() => IsSelected = false;
+
     public abstract void Render(ObjectRenderer renderer);
 }
