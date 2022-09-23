033ceaa3b1324c3996ee06812ef546803fee1a33
Вадим Артюхов <artyuhovvadim@yandex.ru>
2022-09-23 04:09:34 +0300

    Наброски ColorPicker'а

---
+++ LogicSimulator.Controls/ColorPicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace LogicSimulator.Controls;
+
+[TemplatePart(Name = RootPopupName, Type = typeof(Popup))]
+[TemplatePart(Name = RootButtonName, Type = typeof(Button))]
+public class ColorPicker : Control
+{
+    private const string RootPopupName = "PART_RootPopup";
+    private const string RootButtonName = "PART_RootButton";
+
+    private Popup _rootPopup;
+    private Button _rootButton;
+
+    static ColorPicker()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorPicker), new FrameworkPropertyMetadata(typeof(ColorPicker)));
+    }
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        _rootPopup = GetTemplateChild(RootPopupName) as Popup;
+        _rootButton = GetTemplateChild(RootButtonName) as Button;
+
+        if (_rootPopup is null || _rootButton is null)
+            throw new ElementNotAvailableException($"Part element is not available in {GetType()} template!");
+
+        _rootButton.Click += OnRootButtonClick;
+    }
+
+    private void OnRootButtonClick(object sender, RoutedEventArgs e)
+    {
+        _rootPopup.IsOpen = true;
+    }
+}
