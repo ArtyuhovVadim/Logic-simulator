c3581f4798539eb67cdca34122be49ce7b0095df
Вадим Артюхов <artyuhovvadim@yandex.ru>
2022-10-31 04:35:27 +0300

    Добавил ClippingBorder

---
+++ LogicSimulator.Controls/ClippingBorder.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System;
+
+namespace LogicSimulator.Controls;
+
+public class ClippingBorder : Border
+{
+    private readonly RectangleGeometry _clipRect = new();
+    private object _oldClip;
+
+    public override UIElement Child
+    {
+        get => base.Child;
+        set
+        {
+            if (Child == value) return;
+            Child?.SetValue(ClipProperty, _oldClip);
+            _oldClip = value?.ReadLocalValue(ClipProperty);
+            base.Child = value;
+        }
+    }
+
+    protected override void OnRender(DrawingContext dc)
+    {
+        OnApplyChildClip();
+        base.OnRender(dc);
+    }
+
+    protected virtual void OnApplyChildClip()
+    {
+        var child = Child;
+        if (child is null) return;
+        _clipRect.RadiusX = _clipRect.RadiusY = Math.Max(0.0, CornerRadius.TopLeft - BorderThickness.Left * 0.5);
+        _clipRect.Rect = new Rect(Child.RenderSize);
+        child.Clip = _clipRect;
+    }
+}
