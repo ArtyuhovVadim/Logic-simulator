c2c1d85325fc17e263a7cdaf666eb04188c9a482
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-09-09 15:05:35 +0300

    Удалил проект LogicSimulator.AvalonDock и перешел на NuGet пакет

--- LogicSimulator.AvalonDock/Layout/NamespaceDoc.cs
+++
@@ -1,8 +0,0 @@
-namespace AvalonDock.Layout
-{
-	/// <summary>This namespace has the definition and implementation for the layout tree.</summary>
-	[System.Runtime.CompilerServices.CompilerGenerated]
-	internal class NamespaceDoc
-	{
-	}
-}
--- LogicSimulator.AvalonDock/Themes/NamespaceDoc.cs
+++
@@ -1,8 +0,0 @@
-namespace AvalonDock.Themes
-{
-	/// <summary>This namespace holds the base classes and implementations to support custom theme's.</summary>
-	[System.Runtime.CompilerServices.CompilerGenerated]
-	internal class NamespaceDoc
-	{
-	}
-}
