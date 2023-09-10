6e5209e8d1434bbeebd709ea542707cb5bdd101a
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-09-10 17:09:22 +0300

    Добавил конвертер для Rotation

---
+++ LogicSimulator/Infrastructure/Converters/RotationToFloatConverter.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Windows;
+
+namespace LogicSimulator.Infrastructure.Converters;
+
+public class RotationToFloatConverter : Converter
+{
+    public override object Convert(object v, Type t, object p, CultureInfo c)
+    {
+        if (v is not Rotation rotation)
+            return DependencyProperty.UnsetValue;
+
+        return rotation switch
+        {
+            Rotation.Undefined => throw new NotSupportedException(),
+            Rotation.Degrees0 => 0,
+            Rotation.Degrees90 => 90,
+            Rotation.Degrees180 => 180,
+            Rotation.Degrees270 => 270,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    public override object ConvertBack(object v, Type t, object p, CultureInfo c)
+    {
+        if (v is not float rotation)
+            return DependencyProperty.UnsetValue;
+
+        return (int)Math.Abs(rotation) switch
+        {
+            0 => Rotation.Degrees0,
+            90 => Rotation.Degrees90,
+            180 => Rotation.Degrees180,
+            270 => Rotation.Degrees270,
+            _ => Rotation.Undefined
+        };
+    }
+}
