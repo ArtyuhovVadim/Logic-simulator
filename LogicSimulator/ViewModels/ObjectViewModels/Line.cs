b88018dbfe3229f1c778ba6d7a60866e44016d5e
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-09-10 15:49:05 +0300

    Добавил вью модели для линии и изображения

---
+++ LogicSimulator/ViewModels/ObjectViewModels/Line.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Media;
+using LogicSimulator.ViewModels.ObjectViewModels.Base;
+using SharpDX;
+using Color = System.Windows.Media.Color;
+
+namespace LogicSimulator.ViewModels.ObjectViewModels;
+
+public class Line : BaseEditableObjectViewModel
+{
+    #region Vertexes
+
+    private ObservableCollection<Vector2> _vertexes = new();
+
+    public IEnumerable<Vector2> Vertexes
+    {
+        get => _vertexes;
+        private set => Set(ref _vertexes, new ObservableCollection<Vector2>(value));
+    }
+
+    #endregion
+
+    #region StrokeColor
+
+    private Color _strokeColor = Colors.Black;
+
+    public Color StrokeColor
+    {
+        get => _strokeColor;
+        set => Set(ref _strokeColor, value);
+    }
+
+    #endregion
+
+    #region StrokeThickness
+
+    private float _strokeThickness = 1f;
+
+    public float StrokeThickness
+    {
+        get => _strokeThickness;
+        set => Set(ref _strokeThickness, value);
+    }
+
+    #endregion
+}
