e9778de5b30b598fb281ca89bba7c31b2c8bd622
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-11-15 00:39:20 +0300

    Упрстил работу с DataPiping

--- LogicSimulator/Infrastructure/DataPiping/DataPiping.cs
+++ LogicSimulator/Infrastructure/DataPiping/DataPiping.cs
@@ -4,10 +4,18 @@ namespace LogicSimulator.Infrastructure.DataPiping;
 
 public static class DataPiping
 {
-    public static readonly DependencyProperty DataPipesProperty =
-        DependencyProperty.RegisterAttached("DataPipes", typeof(DataPipeCollection), typeof(DataPiping), new UIPropertyMetadata(null));
+    private static readonly DependencyProperty DataPipesProperty =
+        DependencyProperty.RegisterAttached("ShadowDataPipes", typeof(DataPipeCollection), typeof(DataPiping), new FrameworkPropertyMetadata(default(DataPipeCollection)));
 
-    public static void SetDataPipes(DependencyObject o, DataPipeCollection value) => o.SetValue(DataPipesProperty, value);
+    public static DataPipeCollection GetDataPipes(DependencyObject o)
+    {
+        var collection = (DataPipeCollection)o.GetValue(DataPipesProperty);
 
-    public static DataPipeCollection GetDataPipes(DependencyObject o) => (DataPipeCollection)o.GetValue(DataPipesProperty);
+        if (collection is not null) return collection;
+
+        collection = new DataPipeCollection();
+        o.SetValue(DataPipesProperty, collection);
+
+        return collection;
+    }
 }
