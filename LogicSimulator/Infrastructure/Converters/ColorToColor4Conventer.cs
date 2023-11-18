9c51c60f20832fd9085242425c1e77a51ec0ac8b
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-11-18 17:11:24 +0300

    Добавил конвертер цвета

---
+++ LogicSimulator/Infrastructure/Converters/ColorToColor4Conventer.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using LogicSimulator.Utils;
+using SharpDX;
+using Color = System.Windows.Media.Color;
+
+namespace LogicSimulator.Infrastructure.Converters;
+
+public class ColorToColor4Converter : Converter
+{
+    public override object Convert(object v, Type t, object p, CultureInfo c) => ((Color)v!).ToColor4();
+
+    public override object ConvertBack(object v, Type t, object p, CultureInfo c) => ((Color4)v!).ToColor();
+}
