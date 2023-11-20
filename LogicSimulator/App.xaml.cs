7d353964a11d7b8b94b05a4db2e51611158fe6f7
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-11-20 23:06:36 +0300

    Отчистил проект от ненужного кода

--- LogicSimulator/App.xaml.cs
+++ LogicSimulator/App.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.IO;
 using System.Runtime.CompilerServices;
 using System.Windows;
-using LogicSimulator.Controls.Themes;
-using LogicSimulator.Controls.Themes.Dark;
-using LogicSimulator.Controls.Themes.Light;
 using LogicSimulator.Infrastructure.Services;
-using LogicSimulator.ViewModels.Infrastructure;
+using LogicSimulator.Infrastructure.Services.Interfaces;
+using LogicSimulator.ViewModels.AnchorableViewModels;
+using LogicSimulator.ViewModels;
+using LogicSimulator.ViewModels.UserDialogViewModels;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,15 +16,7 @@ public partial class App
 {
     private static IHost _host;
 
-    private static ThemeType _currentTheme = ThemeType.Dark;
-
-    private static readonly Dictionary<ThemeType, Theme> Themes = new()
-    {
-        { ThemeType.Dark, new DarkTheme() },
-        { ThemeType.Light, new LightTheme() }
-    };
-
-    public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+    public static IHost Host => _host ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
 
     public static Window ActiveWindow => Current.Windows.OfType<Window>().FirstOrDefault(w => w.IsActive);
 
@@ -33,27 +26,7 @@ public partial class App
 
     public static bool IsDesignMode { get; private set; } = true;
 
-    public static string CurrentDirectory =>
-        IsDesignMode ? Path.GetDirectoryName(GetSourceCodePath()) : Environment.CurrentDirectory;
-
-    public static ThemeType CurrentTheme
-    {
-        get => _currentTheme;
-        set
-        {
-            if (_currentTheme == value) return;
-
-            var resources = Current.Resources.MergedDictionaries;
-
-            var oldTheme = GetCurrentResourceDictionary();
-            var oldThemeIndex = resources.IndexOf(oldTheme);
-
-            resources.RemoveAt(oldThemeIndex);
-            resources.Insert(oldThemeIndex, new ResourceDictionary { Source = Themes[value].GetResourceUri() });
-
-            _currentTheme = value;
-        }
-    }
+    public static string CurrentDirectory => IsDesignMode ? Path.GetDirectoryName(GetSourceCodePath()) : Environment.CurrentDirectory;
 
     protected override async void OnStartup(StartupEventArgs e)
     {
@@ -74,25 +47,30 @@ public partial class App
         _host.Dispose();
     }
 
-    public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+    private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
     {
         services
-            .RegisterViewModels()
-            .RegisterServices();
+            .AddSingleton<MainWindowViewModel>()
+            .AddSingleton<PropertiesViewModel>()
+            .AddSingleton<ProjectExplorerViewModel>()
+            .AddSingleton<InfoDialogWindowViewModel>()
+            .AddSingleton<ErrorDialogWindowViewModel>()
+            .AddSingleton<WarningDialogWindowViewModel>()
+            .AddSingleton<QuestionDialogWindowViewModel>()
+
+            .AddSingleton<IUserDialogService, UserDialogService>()
+            .AddSingleton<ISchemeFileService, SchemeFileService>()
+            .AddSingleton<IProjectFileService, ProjectFileService>()
+            .AddSingleton<IEditorSelectionService, EditorSelectionService>();
     }
 
-    private static string GetSourceCodePath([CallerFilePath] string path = null)
-    {
-        return path;
-    }
+    private static string GetSourceCodePath([CallerFilePath] string path = null) => path;
 
-    private static ResourceDictionary GetCurrentResourceDictionary()
-    {
-        var resourceDictionary = Current.Resources.MergedDictionaries.FirstOrDefault(x => x.Source.OriginalString.EndsWith("Theme.xaml"));
-
-        if (resourceDictionary is null)
-            throw new ApplicationException("Theme resource not found");
-
-        return resourceDictionary;
-    }
+    private static IHostBuilder CreateHostBuilder(string[] args) => Microsoft.Extensions.Hosting.Host
+        .CreateDefaultBuilder(args)
+        .UseContentRoot(CurrentDirectory)
+        .ConfigureAppConfiguration((_, cfg) => cfg
+            .SetBasePath(CurrentDirectory)
+            .AddJsonFile("app-settings.json", true, true))
+        .ConfigureServices(ConfigureServices);
 }
--- LogicSimulator/Program.cs
+++ LogicSimulator/Program.cs
@@ -1,54 +1,12 @@
-﻿#if !DEBUG
-using System.Windows;
-using LogicSimulator.Infrastructure.Services.Interfaces;
-using Microsoft.Extensions.DependencyInjection;
-#endif
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-
-namespace LogicSimulator;
+﻿namespace LogicSimulator;
 
 public static class Program
 {
     [STAThread]
-    public static void Main(string[] args)
+    public static void Main()
     {
-#if DEBUG
         var app = new App();
         app.InitializeComponent();
         app.Run();
-#else
-        try
-        {
-            var app = new App();
-            app.InitializeComponent();
-            app.Run();
-        }
-        catch (Exception e)
-        {
-            var dialog = App.Host.Services.GetRequiredService<IUserDialogService>();
-
-            var message = $"Message: {e.Message}\nSource: {e.Source}\nStackTrace:\n{e.StackTrace}";
-
-            if (dialog is null)
-            {
-                MessageBox.Show(message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            else
-            {
-                dialog.ShowErrorMessage("Ошибка!", message);
-            }
-        }
-#endif
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-            .UseContentRoot(App.CurrentDirectory)
-            .ConfigureAppConfiguration((_, cfg) => cfg
-                .SetBasePath(App.CurrentDirectory)
-                .AddJsonFile("app-settings.json", true, true))
-            .ConfigureServices(App.ConfigureServices);
     }
 }
