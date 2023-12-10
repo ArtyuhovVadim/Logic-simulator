8fd363704ed13f3516e2e55ebc648fb76b3220f0
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-12-10 23:44:31 +0300

    Улучшил активацию документа при работе сразу с двумя или больше схемами

---
+++ LogicSimulator/Infrastructure/Behaviors/SchemeViewActivationBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using AvalonDock.Controls;
+using LogicSimulator.Views;
+using Microsoft.Xaml.Behaviors;
+using WpfExtensions.Utils;
+
+namespace LogicSimulator.Infrastructure.Behaviors;
+
+public class SchemeViewActivationBehaviour : Behavior<SchemeView>
+{
+    #region IsDocumentActive
+
+    public bool IsDocumentActive
+    {
+        get => (bool)GetValue(IsDocumentActiveProperty);
+        set => SetValue(IsDocumentActiveProperty, value);
+    }
+
+    public static readonly DependencyProperty IsDocumentActiveProperty =
+        DependencyProperty.Register(nameof(IsDocumentActive), typeof(bool), typeof(SchemeViewActivationBehaviour), new PropertyMetadata(default(bool)));
+
+    #endregion
+
+    protected override void OnAttached()
+    {
+        AssociatedObject.PreviewMouseDown += OnMouseDown;
+        AssociatedObject.MouseWheel += OnMouseWheel;
+    }
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject.PreviewMouseDown -= OnMouseDown;
+        AssociatedObject.MouseWheel -= OnMouseWheel;
+    }
+
+    private void OnMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        AssociatedObject.FindVisualParent<LayoutDocumentControl>()!.Model!.IsActive = true;
+    }
+
+    private void OnMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (!IsDocumentActive && e.LeftButton == MouseButtonState.Pressed)
+        {
+            e.Handled = true;
+        }
+
+        AssociatedObject.FindVisualParent<LayoutDocumentControl>()!.Model!.IsActive = true;
+    }
+}
