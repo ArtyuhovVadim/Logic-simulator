7cdd2c6f0803d55e0bb7e7b4d6c36daa03d06e54
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-12-14 22:12:46 +0300

    Добавил методя расширения для YAML

---
+++ LogicSimulator.Utils/YamlParserExtensions.cs
@@ -0,0 +1,27 @@
+﻿using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+
+namespace LogicSimulator.Utils;
+
+public static class YamlParserExtensions
+{
+    public static void BeginSequenceOrThrow(this IParser parser)
+    {
+        if (!parser.TryConsume<SequenceStart>(out _))
+            throw new YamlException($"Invalid YAML at ${parser?.Current?.Start.Line ?? -1} line.");
+    }
+
+    public static void EndSequenceOrThrow(this IParser parser)
+    {
+        if (!parser.TryConsume<SequenceEnd>(out _))
+            throw new YamlException($"Invalid YAML at ${parser?.Current?.Start.Line ?? -1} line.");
+    }
+
+    public static double ConsumeScalarOrThrow(this IParser parser)
+    {
+        if (!parser.TryConsume<Scalar>(out var scalar))
+            throw new YamlException($"Invalid YAML at ${parser?.Current?.Start.Line ?? -1} line.");
+
+        return double.Parse(scalar.Value);
+    }
+}
