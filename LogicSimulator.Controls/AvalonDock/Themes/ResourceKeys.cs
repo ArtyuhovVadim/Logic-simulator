a89372d56d0b5d918ff9225be2bcde8b33a09ead
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-01-02 17:38:40 +0300

    Сгруппировал кисти и стили

--- LogicSimulator.Controls/AvalonDock/Themes/ResourceKeys.cs
+++ LogicSimulator.Controls/AvalonDock/Themes/ResourceKeys.cs
@@ -5,61 +5,51 @@ namespace LogicSimulator.Controls.AvalonDock.Themes;
 
 public static class ResourceKeys
 {
-    public static readonly ComponentResourceKey ControlAccentColorKey = CreateInstance();
-
     public static readonly ComponentResourceKey Background = CreateInstance();
     public static readonly ComponentResourceKey PanelBorderBrush = CreateInstance();
     public static readonly ComponentResourceKey TabBackground = CreateInstance();
+    public static readonly ComponentResourceKey MenuItemIconBrush = CreateInstance();
+
+    public static readonly ComponentResourceKey ToolButtonBackground = CreateInstance();
+    public static readonly ComponentResourceKey ToolButtonForeground = CreateInstance();
+    public static readonly ComponentResourceKey ToolButtonHoveredBackground = CreateInstance();
+    public static readonly ComponentResourceKey ToolButtonHoveredForeground = CreateInstance();
+    public static readonly ComponentResourceKey ToolButtonCheckedBackground = CreateInstance();
+    public static readonly ComponentResourceKey ToolButtonCheckedForeground = CreateInstance();
+    public static readonly ComponentResourceKey ToolButtonPressedBackground = CreateInstance();
+    public static readonly ComponentResourceKey ToolButtonPressedForeground = CreateInstance();
+
+    public static readonly ComponentResourceKey ButtonBackground = CreateInstance();
+    public static readonly ComponentResourceKey ButtonForeground = CreateInstance();
+    public static readonly ComponentResourceKey ButtonHoveredBackground = CreateInstance();
+    public static readonly ComponentResourceKey ButtonHoveredForeground = CreateInstance();
+    public static readonly ComponentResourceKey ButtonPressedBackground = CreateInstance();
+    public static readonly ComponentResourceKey ButtonPressedForeground = CreateInstance();
 
     public static readonly ComponentResourceKey AutoHideTabDefaultBackground = CreateInstance();
     public static readonly ComponentResourceKey AutoHideTabDefaultBorder = CreateInstance();
-    public static readonly ComponentResourceKey AutoHideTabDefaultText = CreateInstance();
-
     public static readonly ComponentResourceKey AutoHideTabHoveredBackground = CreateInstance();
 
-    public static readonly ComponentResourceKey DocumentWellTabSelectedActiveBackground = CreateInstance();
-
-    public static readonly ComponentResourceKey DocumentWellTabSelectedActiveText = CreateInstance();
-    public static readonly ComponentResourceKey DocumentWellTabSelectedInactiveBackground = CreateInstance();
-    public static readonly ComponentResourceKey DocumentWellTabSelectedInactiveText = CreateInstance();
-    public static readonly ComponentResourceKey DocumentWellTabUnselectedBackground = CreateInstance();
-    public static readonly ComponentResourceKey DocumentWellTabUnselectedText = CreateInstance();
-
-    public static readonly ComponentResourceKey DocumentWellTabUnselectedHoveredBackground = CreateInstance();
-    public static readonly ComponentResourceKey DocumentWellTabUnselectedHoveredText = CreateInstance();
+    public static readonly ComponentResourceKey DocumentTabSelectedBackground = CreateInstance();
+    public static readonly ComponentResourceKey DocumentTabUnselectedBackground = CreateInstance();
+    public static readonly ComponentResourceKey DocumentTabUnselectedHoveredBackground = CreateInstance();
 
     public static readonly ComponentResourceKey ToolWindowCaptionActiveBackground = CreateInstance();
-    public static readonly ComponentResourceKey ToolWindowCaptionActiveText = CreateInstance();
     public static readonly ComponentResourceKey ToolWindowCaptionInactiveBackground = CreateInstance();
-    public static readonly ComponentResourceKey ToolWindowCaptionInactiveText = CreateInstance();
-
-    public static readonly ComponentResourceKey ToolWindowCaptionButtonInactiveGlyph = CreateInstance();
-
-    public static readonly ComponentResourceKey ToolWindowTabSelectedActiveBackground = CreateInstance();
-
-    public static readonly ComponentResourceKey ToolWindowTabSelectedActiveText = CreateInstance();
-    public static readonly ComponentResourceKey ToolWindowTabSelectedInactiveBackground = CreateInstance();
-
-    public static readonly ComponentResourceKey ToolWindowTabSelectedInactiveText = CreateInstance();
+    public static readonly ComponentResourceKey ToolWindowTabSelectedBackground = CreateInstance();
     public static readonly ComponentResourceKey ToolWindowTabUnselectedBackground = CreateInstance();
-    public static readonly ComponentResourceKey ToolWindowTabUnselectedText = CreateInstance();
     public static readonly ComponentResourceKey ToolWindowTabUnselectedHoveredBackground = CreateInstance();
-
-    public static readonly ComponentResourceKey ToolWindowTabUnselectedHoveredText = CreateInstance();
 
     public static readonly ComponentResourceKey FloatingDocumentWindowBackground = CreateInstance();
     public static readonly ComponentResourceKey FloatingDocumentWindowBorder = CreateInstance();
     public static readonly ComponentResourceKey FloatingDocumentActiveWindowBorder = CreateInstance();
-
     public static readonly ComponentResourceKey FloatingToolWindowBackground = CreateInstance();
     public static readonly ComponentResourceKey FloatingToolWindowBorder = CreateInstance();
 
     public static readonly ComponentResourceKey DockingButtonWidthKey = CreateInstance();
     public static readonly ComponentResourceKey DockingButtonHeightKey = CreateInstance();
-
     public static readonly ComponentResourceKey DockingButtonForegroundBrushKey = CreateInstance();
     public static readonly ComponentResourceKey DockingButtonForegroundArrowBrushKey = CreateInstance();
-
     public static readonly ComponentResourceKey DockingButtonStarBorderBrushKey = CreateInstance();
     public static readonly ComponentResourceKey DockingButtonStarBackgroundBrushKey = CreateInstance();
 
