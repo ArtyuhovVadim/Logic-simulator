9dc988b72df18e03277ac7b538e8954e292705c9
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-01-03 22:05:45 +0300

    Настроит глобальный отлов ошибок

--- LogicSimulator/App.xaml.cs
+++ LogicSimulator/App.xaml.cs
@@ -26,13 +26,17 @@ public partial class App
 
     public static string CurrentDirectory => IsDesignMode ? Path.GetDirectoryName(GetSourceCodePath())! : Environment.CurrentDirectory;
 
-    protected override async void OnStartup(StartupEventArgs e)
+    protected override async void OnStartup(StartupEventArgs args)
     {
-        base.OnStartup(e);
+        base.OnStartup(args);
 
         IsDesignMode = false;
 
         Environment.CurrentDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly()!.Location)!;
+
+#if !DEBUG
+        SetupGlobalExceptionHandling();
+#endif
 
         await Host.StartAsync().ConfigureAwait(false);
 
@@ -65,6 +69,41 @@ public partial class App
             .AddSingleton<ISchemeViewModelFactory, SchemeViewModelFactory>()
             .AddSingleton<IProjectViewModelFactory, ProjectViewModelFactory>()
             ;
+    }
+
+    private static void SetupGlobalExceptionHandling()
+    {
+        try
+        {
+            // handles non-UI thread exceptions thrown; the app terminates after unhandled exceptions are caught here
+            AppDomain.CurrentDomain.UnhandledException += (_, e) => HandleException((Exception)e.ExceptionObject, e.IsTerminating);
+
+            // handles UI dispatcher thread exceptions thrown
+            Current.DispatcherUnhandledException += (_, e) => e.Handled = HandleException(e.Exception);
+
+            // handles domain-wide exceptions where a task scheduler is used for asynchronous operations
+            TaskScheduler.UnobservedTaskException += (_, e) => { if (HandleException(e.Exception)) e.SetObserved(); };
+        }
+        catch (Exception e)
+        {
+            throw new ApplicationException($"Unable to use global exception handling.\n{e.Message}");
+        }
+    }
+
+    private static bool HandleException(Exception exception, bool isTerminating = false)
+    {
+        if (isTerminating)
+        {
+            MessageBox.Show(exception.ToString(), "Critical error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            Current.Shutdown();
+
+            return false;
+        }
+
+        MessageBox.Show(exception.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+        return true;
     }
 
     private static string GetSourceCodePath([CallerFilePath] string? path = null) => path!;
