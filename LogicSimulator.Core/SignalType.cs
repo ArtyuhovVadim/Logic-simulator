f3334336c3f072d06d64975f00bb94b8ab2d9dff
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-03-08 20:38:41 +0300

    Добавил алфавит и SignalsCalculator

---
+++ LogicSimulator.Core/SignalType.cs
@@ -0,0 +1,34 @@
+﻿namespace LogicSimulator.Core;
+
+public enum SignalType
+{
+    /// <summary>
+    /// Низкий сигнал
+    /// </summary>
+    Low,
+
+    /// <summary>
+    /// Высокий сигнал
+    /// </summary>
+    High,
+
+    /// <summary>
+    /// Сигнал не определён, т.е. 0 или 1
+    /// </summary>
+    Undefined,
+
+    /// <summary>
+    /// Передний фронт, т.е. 0 => 1
+    /// </summary>
+    PosEdge,
+
+    /// <summary>
+    /// Задний фронт, т.е. 1 => 0
+    /// </summary>
+    NegEdge,
+
+    /// <summary>
+    /// Сигнал высокого импеданса
+    /// </summary>
+    HighImp,
+}
