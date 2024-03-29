bfa20fbce9576b4a96f0989aff4a16e9ed1df090
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-03-29 22:35:28 +0300

    Методы расширения

---
+++ LogicSimulator.Utils/RectangleFExtensionMethods.cs
@@ -0,0 +1,8 @@
+﻿using SharpDX;
+
+namespace LogicSimulator.Utils;
+
+public static class RectangleFExtensionMethods
+{
+    public static RectangleF ScaleSize(this RectangleF rect, float scale) => rect with { Width = rect.Width * scale, Height = rect.Height * scale };
+}
