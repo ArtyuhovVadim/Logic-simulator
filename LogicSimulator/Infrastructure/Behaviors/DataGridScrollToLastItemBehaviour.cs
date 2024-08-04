23880f7050a65a69d808bc893649c2276e1bff59
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-08-04 16:39:43 +0300

    Добавил поведение DataGrid для автоматической прокрутки до последнего добавленного элемента коллекции

---
+++ LogicSimulator/Infrastructure/Behaviors/DataGridScrollToLastItemBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace LogicSimulator.Infrastructure.Behaviors;
+
+public class DataGridScrollToLastItemBehaviour : Behavior<DataGrid>
+{
+    protected override void OnAttached()
+    {
+        if (!AssociatedObject.IsLoaded)
+        {
+            AssociatedObject.Loaded += OnLoaded;
+            return;
+        }
+
+        if (AssociatedObject.ItemsSource is not INotifyCollectionChanged collection) return;
+
+        collection.CollectionChanged += OnCollectionChanged;
+    }
+
+    protected override void OnDetaching()
+    {
+        if (AssociatedObject.ItemsSource is not INotifyCollectionChanged collection) return;
+
+        collection.CollectionChanged -= OnCollectionChanged;
+    }
+
+    private void OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action == NotifyCollectionChangedAction.Add)
+        {
+            AssociatedObject.ScrollIntoView(AssociatedObject.ItemsSource.Cast<object>().Last());
+        }
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (AssociatedObject.ItemsSource is not INotifyCollectionChanged collection) return;
+
+        collection.CollectionChanged += OnCollectionChanged;
+        AssociatedObject.Loaded -= OnLoaded;
+    }
+}
