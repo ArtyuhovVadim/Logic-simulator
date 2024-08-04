163fcaf007aa4a98b8d96b2b2bae696c5e030c8d
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-08-04 16:40:20 +0300

    Добавил комманду, копирующую текст из CommandParameter в буфер обмена

---
+++ LogicSimulator/Infrastructure/Commands/CopyToClipboardCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using WpfExtensions.Mvvm.Commands.Base;
+
+namespace LogicSimulator.Infrastructure.Commands;
+
+public class CopyToClipboardCommand : BaseCommand
+{
+    protected override void OnExecute(object? parameter) => Clipboard.SetText(parameter!.ToString()!, TextDataFormat.UnicodeText);
+
+    protected override bool OnCanExecute(object? parameter) => parameter is not null;
+}
