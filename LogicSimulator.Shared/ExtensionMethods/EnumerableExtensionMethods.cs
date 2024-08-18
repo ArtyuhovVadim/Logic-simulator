c94f29773efc261a04f7c009cb7ccd854c58044a
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-08-18 22:10:44 +0300

    Добавлены методы расширения

---
+++ LogicSimulator.Shared/ExtensionMethods/EnumerableExtensionMethods.cs
@@ -0,0 +1,8 @@
+﻿namespace LogicSimulator.Shared.ExtensionMethods;
+
+public static class EnumerableExtensionMethods
+{
+    public static string ToStr<T>(this IEnumerable<T> enumerable) => $"[{string.Join(", ", enumerable)}]";
+
+    public static string ToStr<T>(this IEnumerable<T> enumerable, Func<T, string> formatter) => $"[{string.Join(", ", enumerable.Select(formatter))}]";
+}
---
+++ LogicSimulator.Shared/ExtensionMethods/StringExtensionMethods.cs
@@ -0,0 +1,6 @@
+﻿namespace LogicSimulator.Shared.ExtensionMethods;
+
+public static class StringExtensionMethods
+{
+    public static string ReturnIfNotEmpty(this string str, string alt = " ") => string.IsNullOrWhiteSpace(str) ? alt : $" {str} ";
+}
