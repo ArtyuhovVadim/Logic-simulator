7ce8885d41f6d808746b9d851af16af670bb2840
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-08-19 13:52:45 +0300

    Добвил ObjectToEnumerableConverter

---
+++ LogicSimulator/Infrastructure/Converters/ObjectToEnumerableConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using SharpDX;
+using WpfExtensions.Converters.Base;
+using Color = System.Windows.Media.Color;
+
+namespace LogicSimulator.Infrastructure.Converters;
+
+[ValueConversion(typeof(Color), typeof(Color4))]
+[MarkupExtensionReturnType(typeof(ColorToColor4Converter))]
+public class ObjectToEnumerableConverter : BaseConverter<object?, IEnumerable<object?>>
+{
+    public override IEnumerable<object?> Convert(object? value, object? parameter, CultureInfo culture) => [value];
+
+    public override object? ConvertBack(IEnumerable<object?>? value, object? parameter, CultureInfo culture) => value?.First();
+}
