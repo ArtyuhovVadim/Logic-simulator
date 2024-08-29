c77ea5b543769a3aed70147927470f681db13608
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-08-29 15:03:37 +0300

    Исправил баг в RotationToFloatConverter

--- LogicSimulator/Infrastructure/Converters/RotationToFloatConverter.cs
+++ LogicSimulator/Infrastructure/Converters/RotationToFloatConverter.cs
@@ -1,5 +1,4 @@
 ﻿using System.Globalization;
-using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
 using LogicSimulator.Models.Common;
@@ -7,37 +6,25 @@ using WpfExtensions.Converters.Base;
 
 namespace LogicSimulator.Infrastructure.Converters;
 
-[ValueConversion(typeof(Rotation), typeof(int))]
+[ValueConversion(typeof(Rotation), typeof(float))]
 [MarkupExtensionReturnType(typeof(RotationToFloatConverter))]
-public class RotationToFloatConverter : BaseConverter
+public class RotationToFloatConverter : BaseConverter<Rotation, float>
 {
-    public override object Convert(object? v, Type t, object? p, CultureInfo c)
+    public override float Convert(Rotation value, object? parameter, CultureInfo culture) => value switch
     {
-        if (v is not Rotation rotation)
-            return DependencyProperty.UnsetValue;
+        Rotation.Degrees0 => 0,
+        Rotation.Degrees90 => 90,
+        Rotation.Degrees180 => 180,
+        Rotation.Degrees270 => 270,
+        _ => throw new ArgumentOutOfRangeException()
+    };
 
-        return rotation switch
-        {
-            Rotation.Degrees0 => 0,
-            Rotation.Degrees90 => 90,
-            Rotation.Degrees180 => 180,
-            Rotation.Degrees270 => 270,
-            _ => throw new ArgumentOutOfRangeException()
-        };
-    }
-
-    public override object ConvertBack(object? v, Type t, object? p, CultureInfo c)
+    public override Rotation ConvertBack(float value, object? parameter, CultureInfo culture) => (int)Math.Abs(value) switch
     {
-        if (v is not float rotation)
-            return DependencyProperty.UnsetValue;
-
-        return (int)Math.Abs(rotation) switch
-        {
-            0 => Rotation.Degrees0,
-            90 => Rotation.Degrees90,
-            180 => Rotation.Degrees180,
-            270 => Rotation.Degrees270,
-            _ => throw new ArgumentOutOfRangeException()
-        };
-    }
+        0 => Rotation.Degrees0,
+        90 => Rotation.Degrees90,
+        180 => Rotation.Degrees180,
+        270 => Rotation.Degrees270,
+        _ => throw new ArgumentOutOfRangeException()
+    };
 }
