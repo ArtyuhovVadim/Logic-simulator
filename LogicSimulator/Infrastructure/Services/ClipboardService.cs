26713fd2653322b8ede709984c886f4fc5faad8f
Вадим Артюхов <artyuhovvadim@yandex.ru>
2024-08-30 15:30:37 +0300

    Добавил ClipboardService

---
+++ LogicSimulator/Infrastructure/Services/ClipboardService.cs
@@ -0,0 +1,19 @@
+﻿using LogicSimulator.Infrastructure.Services.Interfaces;
+using LogicSimulator.ViewModels.Objects.Base;
+
+namespace LogicSimulator.Infrastructure.Services;
+
+public class ClipboardService : IClipboardService
+{
+    private List<BaseObjectViewModel> _buffer = [];
+
+    public bool HasCopiedObjects => _buffer.Count > 0;
+
+    public void Copy(IEnumerable<BaseObjectViewModel> objects) => _buffer = Clone(objects);
+
+    public List<BaseObjectViewModel> Paste() => Clone(_buffer);
+
+    public List<BaseObjectViewModel> Duplicate(IEnumerable<BaseObjectViewModel> objects) => Clone(objects);
+
+    private List<BaseObjectViewModel> Clone(IEnumerable<BaseObjectViewModel> objects) => objects.Select(x => x.MakeClone()).ToList();
+}
---
+++ LogicSimulator/Infrastructure/Services/Interfaces/IClipboardService.cs
@@ -0,0 +1,14 @@
+﻿using LogicSimulator.ViewModels.Objects.Base;
+
+namespace LogicSimulator.Infrastructure.Services.Interfaces;
+
+public interface IClipboardService
+{
+    bool HasCopiedObjects { get; }
+
+    void Copy(IEnumerable<BaseObjectViewModel> objects);
+
+    List<BaseObjectViewModel> Paste();
+
+    List<BaseObjectViewModel> Duplicate(IEnumerable<BaseObjectViewModel> objects);
+}
